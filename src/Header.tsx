10c2870de25d2713109490b42b842448f621b848
rafajperez <rafael_cordas@hotmail.com>
2026-09-16 21:15:58 -0300

    build inicial

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="bg-[#161410]">
+      <div className="mx-auto flex w-full items-center justify-between p-3 md:w-184.25 md:p-0">
+        <img src="./logo.png" alt="" />
+        <div className="flex h-[35] w-32.5 items-center justify-center rounded-sm bg-[#f2DAAC]">
+          Entrar
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
